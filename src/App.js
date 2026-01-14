c331484941497e37fd05ef4e96a08601bf53359b
eliel <elieljrdl1@gmail.com>
2026-01-13 21:28:58 -0300

    criando os components do header

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
 import './App.css';
-import Logo from './components/Logo';
-import perfil from './imagens/perfil.svg';
-import sacola from './imagens/sacola.svg';
-
-const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE'];
-const icones = [perfil, sacola];
-
+import Header from './components/Header';
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <Logo />
-        <ul className='opcoes'>
-          { textoOpcoes.map( (texto) => (
-            <li className='opcao'><p>{texto}</p></li>
-          )) }
-        </ul>
-
-        <ul className='icones'>
-          { icones.map((icone) => (
-            <li className='icone'><img src={icone} alt='icones'></img></li>
-          )) }
-        </ul>
-      </header>
+      <Header />
     </div>
   );
 }
