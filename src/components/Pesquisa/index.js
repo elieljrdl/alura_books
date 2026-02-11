d7870f6f05adbf46f7e7015e9e93f12787ff2075
eliel <elieljrdl1@gmail.com>
2026-02-10 22:59:40 -0300

    finalizando favoritos

--- src/components/Pesquisa/index.js
+++ src/components/Pesquisa/index.js
@@ -2,6 +2,7 @@ import Input from '../Input/index'
 import styled from 'styled-components'
 import { useEffect, useState } from 'react'
 import { getLivros } from '../../services/livros'
+import { postFavorito } from '../../services/favoritos'
 
 
 const PesquisaContainer = styled.section`
@@ -51,6 +52,11 @@ function Pesquisa() {
         setLivros(livrosDaAPI)
     }
 
+    async function insertFavorito(id) {
+        await postFavorito(id)
+        alert(`Livro de id:${id} inserido`)
+    }
+
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar?</Titulo>
@@ -65,7 +71,7 @@ function Pesquisa() {
             />
 
             { livrosPesquisados.map( livro => (
-                <Resultado>
+                <Resultado onClick={() => insertFavorito(livro.id)}>
                     <p>{livro.nome}</p>
                     <img src={livro.src} alt='livro'/>
                 </Resultado>
